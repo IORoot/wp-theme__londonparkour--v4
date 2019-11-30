a11bb5465562cd10995e8af864940e183394a7a5
Andy Pearson <andy@londonparkour.com>
2019-11-30 09:25:14 +0000

    Add title metatag

--- header.php
+++ header.php
@@ -16,21 +16,19 @@
 	//  └─────────────────────────────────────────────────────────────────────────┘░
 	//   ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
 	$amp_link = get_field('amp_link');
-
+	$body_classes = get_field('body_classes');
+	$title_metatag = get_field('title_metatag')
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
+	<title><?php if ($title_metatag != null){ echo $title_metatag; } else { echo wp_title() . '- LondonParkour'; } ?></title>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 	<link rel="amphtml" href="<?php echo $amp_link; ?>">
 	<?php wp_head(); ?>
 </head>
-
-<?php
-	$body_classes = get_field('body_classes');
-?>
 <body <?php body_class('londonparkour ' . $body_classes ); ?>>
 <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P5T257F" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <div id="progress"><div id="progress__bar"></div></div>
