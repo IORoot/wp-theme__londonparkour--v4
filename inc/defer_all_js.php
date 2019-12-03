7de000b578589703a67d1ff3b2f15371f76d9a6c
Andy Pearson <andy@londonparkour.com>
2019-12-03 08:28:47 +0000

    ACF Scripts load without defer

--- inc/defer_all_js.php
+++ inc/defer_all_js.php
@@ -4,7 +4,12 @@
 if (!(is_admin())) {
     function defer_js($url) {
         if (FALSE === strpos($url, '.js')) return $url;
-        if (strpos($url, 'jquery.js')) return $url;
+
+        // Exception list
+        if (strpos($url, 'jquery.js')) return $url;                 // JQuery needed to be loaded up first.
+        if (strpos($url, 'acf-input.min.js')) return $url;          // ACF needed to be loaded before the contact page scripts.
+        if (strpos($url, 'acf-pro-input.min.js')) return $url;      // ACF needed to be loaded before the contact page scripts.
+        
         return "$url' defer onload='";
     }
     add_filter('clean_url', 'defer_js', 11, 1);
