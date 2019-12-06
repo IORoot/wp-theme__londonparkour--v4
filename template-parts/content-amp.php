c76692833dd16ef56e7862db36f4fea25e3376a5
Andy Pearson <andy@londonparkour.com>
2019-12-06 13:55:23 +0000

    Add AMP trigger for W3TC

--- template-parts/content-amp.php
+++ template-parts/content-amp.php
@@ -12,6 +12,18 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
 	<?php
+
+	//  ┌─────────────────────────────────────────────────────────────────────────┐
+	//  │ Manually set this AMP function.                                         │
+	//  │                                                                         │
+	//  │ Background:                                                             │
+	//  │ W3 Total Cache - AMP Plugin searches for a function called              │
+	//  │ is_amp_endpoint() and expects it to return true if on an AMP page. This │
+	//  │ comes with the AMPforWP plugin, but not using that. So manually setting │
+	//  │ it to make sure the CSS is inline, not linked.                          │
+	//  └─────────────────────────────────────────────────────────────────────────┘
+	function is_amp_endpoint(){ return true; }
+
 	the_content();
 	?>
 
