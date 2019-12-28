5b9f2facb3cfc3d2a2319bf2eee7c326fce3718f
Andy Pearson <andy@londonparkour.com>
2019-12-28 08:35:18 +0000

    Include youtube-lite

--- inc/enqueue_scripts.php
+++ inc/enqueue_scripts.php
@@ -7,7 +7,8 @@ function londonparkourv4_scripts() {
 	wp_enqueue_script( 'londonparkourv4-lazysizes-bgset', get_template_directory_uri() . '/js/lazysizes_plugins/ls.bgset.min.js', array(), null, true );
 	wp_enqueue_script( 'londonparkourv4-lazysizes-unveil', get_template_directory_uri() . '/js/lazysizes_plugins/ls.unveilhooks.min.js', array(), null, true );
     wp_enqueue_script( 'londonparkourv4-lazysizes', get_template_directory_uri() . '/js/lazysizes.min.js', array(), null, true );
-    
+	wp_enqueue_script( 'londonparkourv4-lite-yt', get_template_directory_uri() . '/js/youtube-lite/lite-yt-embed.js', array(), null, true );
+	
 	wp_enqueue_script( 'londonparkourv4-tagmanager', get_template_directory_uri() . '/js/google_tag_manager.js', array(), null, false );
 
 	// DESKTOP ONLY SCRIPTS
