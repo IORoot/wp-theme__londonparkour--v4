17484e1dc6ffe47c4e1e04b6a6148eaa28449907
Andy Pearson <andy@londonparkour.com>
2020-02-10 08:55:07 +0000

    Add RankMath Breadcrumbs

--- components/_breadcrumbs.php
+++ components/_breadcrumbs.php
@@ -10,8 +10,14 @@
 *    ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
 */                                                                              
 
+//YOAST
 if ( function_exists('yoast_breadcrumb') ) {
     yoast_breadcrumb( '<div class="article__breadcrumbs"><p id="breadcrumbs">','</p></div>' );
+}
+
+//RANKMATH
+if( function_exists( 'rank_math_get_breadcrumbs' ) ) {
+    echo '<div class="article__breadcrumbs"><p id="breadcrumbs">' . rank_math_get_breadcrumbs() . '</p></div>';
 }
 
 ?>
--- page_article.php
+++ page_article.php
@@ -13,6 +13,10 @@ get_header();
 	if ( function_exists('yoast_breadcrumb') ) {
 		yoast_breadcrumb( '<div class="article__breadcrumbs"><p id="breadcrumbs">','</p></div>' );
 	}
+	//RANKMATH
+	if( function_exists( 'rank_math_get_breadcrumbs' ) ) {
+		echo '<div class="article__breadcrumbs"><p id="breadcrumbs">' . rank_math_get_breadcrumbs() . '</p></div>';
+	}
 	?>
 
 	<?php
