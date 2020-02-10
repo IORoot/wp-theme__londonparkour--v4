dac0527b17392d7cd87deebeb1b5342417952be4
Andy Pearson <andy@londonparkour.com>
2020-02-10 09:06:24 +0000

    Add RankMath to taxonomy page

--- taxonomy-articlecategory.php
+++ taxonomy-articlecategory.php
@@ -12,7 +12,11 @@
 	if ( function_exists('yoast_breadcrumb') ) {
 		yoast_breadcrumb( '<div class="article__breadcrumbs"><p id="breadcrumbs">','</p></div>' );
     }
-    
+
+    //RANKMATH
+	if( function_exists( 'rank_math_get_breadcrumbs' ) ) {
+		echo '<div class="article__breadcrumbs"><p id="breadcrumbs">' . rank_math_get_breadcrumbs() . '</p></div>';
+	}
     
 ?> 
 
