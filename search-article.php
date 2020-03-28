8dd170fbdedcf14eaeae307a5f1d68c944d9b4fe
Andy Pearson <andy@londonparkour.com>
2020-03-28 08:44:51 +0000

    Add search result design and sass

--- search-article.php
+++ search-article.php
@@ -17,24 +17,28 @@
 
 get_header();
 
-include get_template_directory().'/components/_breadcrumbs.php';
-
 ?>
-<div class="articlesearch">
-
 	<section id="primary" class="content-area">
 		<main id="main" class="site-main">
-
-		<?php
-			do_shortcode('[articles_searchbar]'); 
-			//include get_template_directory().'/components/_search.php'; 
-		?>
 
 		<?php if ( have_posts() ) : ?>
 
 			<h2 class="searchresults-title">
-				<?php printf( esc_html__( 'Search Results for: %s', 'londonparkourv4' ), '<span>' . get_search_query() . '</span>' ); ?>
+				<?php 
+					printf( esc_html__( 'Search Results for: %s', 'londonparkourv4' ), '<span>' . get_search_query() . '</span>' ); 
+
+					echo '<span class="searchresults__count">' . $wp_query->found_posts . ' results</span>';
+					
+				?>
 			</h2>
+
+			
+			<div class="searchresults__search-bar"><?php do_shortcode('[articles_searchbar]'); ?></div>
+
+
+			<?php include get_template_directory().'/components/_pagination.php'; ?>
+
+			
 			<div class="searchresults">
 				<?php
 
@@ -46,18 +50,20 @@ include get_template_directory().'/components/_breadcrumbs.php';
 
 				?>
 			</div>
-		<?php 
-			include get_template_directory().'/components/_pagination.php';
 
-		else :
-			get_template_part( 'template-parts/content', 'none' );
+			<?php include get_template_directory().'/components/_pagination.php'; ?>
 
-		endif;
-		?>
+		<?php else : get_template_part( 'template-parts/content', 'none' ); ?>
+
+		<?php endif; ?>
 
 		</main><!-- #main -->
 	</section><!-- #primary -->
-</div>
+
+<!-- #sidebar -->
+<aside id="secondary" class="widget-area">
+	<?php  do_shortcode('[andyp_responsive_menus menu="Tutorials" sidebar="sidebar-tutorials"]'); ?>
+</aside>
 
 <?php
 get_footer();
--- template-parts/content-search.php
+++ template-parts/content-search.php
@@ -6,14 +6,42 @@
  *
  * @package londonparkour.com_v4
  */
+
+	$id = $post->ID;
+	$link = $post->guid;
+	$title = $post->post_title;
+	$image = get_the_post_thumbnail_url($id);
+	$content = mb_strimwidth( strip_tags($post->post_content) , 0,200, '...');
+	$date = human_time_diff( get_the_time( 'U', $id ), current_time( 'timestamp' ) );
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class('liftup'); ?>>
-	<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-	
-	<?php londonparkourv4_post_thumbnail(); ?>
+<article class="andyp-search__cell">
+	<div class="pushin">
+		<a class="andyp-search__cell-link" href="<?php echo $link; ?>">
 
-	<div class="entry-summary">
-		<?php the_excerpt(); ?>
+			<div class="andyp-search__cell-meta andyp-search__cell-overlay">
+				<i class="andyp-search__cell-icon material-icons">remove_red_eye</i>
+			</div>
+
+			<div class="andyp-search__cell-content">
+
+				<img class="andyp-search__cell-meta andyp-search__cell-image lazyload" src="<?php echo $image; ?>" >
+
+				<div class="andyp-search__cell-info">
+
+					<div class="andyp-search__cell-meta andyp-search__cell-title ">
+						<i class="andyp-search__cell-icon material-icons">play_circle_outline</i>
+						<?php echo $title; ?>
+					</div>
+
+					<div class="andyp-search__cell-meta andyp-search__cell-description"><?php echo $content; ?></div>
+
+					<!-- DATE -->
+					<div class="andyp-search__cell-meta andyp-search__cell-date"><?php echo $date; ?> ago.</div>
+
+				</div>
+
+			</div>
+		</a>
 	</div>
 </article>
