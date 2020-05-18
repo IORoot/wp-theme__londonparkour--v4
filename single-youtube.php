660bdaeb97706fe3de6edc0f1ef54d506cb954ee
Andy Pearson <andy@londonparkour.com>
2020-05-18 14:54:34 +0100

    Add modal functionality

---
+++ single-youtube.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * The template for displaying all single posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package londonparkour.com_v4
+ */
+
+get_header();
+
+?>
+
+<style>
+	.site-content {margin:58px 8.33%;}
+	#masthead, footer {display:none;}
+</style>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+
+		<?php
+		while ( have_posts() ) :	
+			the_post();
+
+			get_template_part( 'template-parts/content', get_post_type() );
+
+		endwhile; // End of the loop.
+		?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+// get_sidebar();
+get_footer();
---
+++ template-parts/content-youtube.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Template part for displaying MODALS for the YouTube CPT on the scraper.
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+	<div class="entry-content">
+		<?php
+		the_content( sprintf(
+			wp_kses(
+				/* translators: %s: Name of current post. Only visible to screen readers */
+				__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'londonparkourv4' ),
+				array(
+					'span' => array(
+						'class' => array(),
+					),
+				)
+			),
+			get_the_title()
+		) );
+
+		?>
+	</div><!-- .entry-content -->
+
+</article><!-- #post-<?php the_ID(); ?> -->
