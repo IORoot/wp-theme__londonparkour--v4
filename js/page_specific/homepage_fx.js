ec04745d6e7a90ecf8c3254e874993c1337b463d
Andy Pearson <andy@londonparkour.com>
2020-06-07 09:29:14 +0100

    Remove PT blockouttext

--- js/page_specific/homepage_fx.js
+++ js/page_specific/homepage_fx.js
@@ -14,8 +14,8 @@ anime__punchin('.c-hero-panel__fly .c-hero-panel__xxl', 150, 'L');
 anime__punchin('.c-hero-panel__fly .c-hero-panel__eyebrow', 200, 'L');
 
 // // PT
-anime__blockoutText('.c-hero-panel__pt .c-hero-panel__content h4');
-anime__blockoutText('.c-hero-panel__pt .c-hero-panel__content p', 500);
+// anime__blockoutText('.c-hero-panel__pt .c-hero-panel__content h4');
+// anime__blockoutText('.c-hero-panel__pt .c-hero-panel__content p', 500);
 
 // PULSE
 anime__isometricScroll('.c-hero-panel__pulse > .c-hero-panel__overlay');
