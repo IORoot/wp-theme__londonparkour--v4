784e49ca1f77ddaf37d1fb66187cf68c4462f9ab
Andy Pearson <andy@londonparkour.com>
2020-06-09 16:20:07 +0100

    update

--- js/page_specific/private_fx.js
+++ js/page_specific/private_fx.js
@@ -2,8 +2,8 @@
  *  Private Sessions Anime FX
 */
 
-anime__punchin('.c-hero-panel__header-private .c-hero-panel__overlay', 50, 'R');
-anime__punchin('.c-hero-panel__header-private .c-hero-panel__xxl', 150, 'T');
-anime__punchin('.c-hero-panel__header-private .c-hero-panel__description', 200, 'L');
+anime__punchin('.heropanel__header-private .heropanel__overlay', 50, 'R');
+anime__punchin('.heropanel__header-private .heropanel__xxl', 150, 'T');
+anime__punchin('.heropanel__header-private .heropanel__description', 200, 'L');
 
 // anime__morphpixels('.c-hero-panel__header-private #pixelGrid #Pixel');
