d247ef0fd2eb97e1f3ca01e8762202abc5aaece3
Andy Pearson <andy@londonparkour.com>
2020-06-09 08:40:11 +0100

    Change ANIME targets

--- js/page_specific/homepage_fx.js
+++ js/page_specific/homepage_fx.js
@@ -3,19 +3,19 @@
 */
 
 // Background
-// anime__punchin('.c-hero-panel__fly',0,'L');
-// anime__punchin('.c-hero-panel__pt', 300, 'R');
-// anime__punchin('.c-hero-panel__pulse',600, 'B');
+// anime__punchin('.heropanel__fly',0,'L');
+// anime__punchin('.heropanel__pt', 300, 'R');
+// anime__punchin('.heropanel__pulse',600, 'B');
 
 // FLY
-anime__fadeinX('.c-hero-panel__fly .c-hero-panel__button',2200, -50);
-anime__punchin('.c-hero-panel__fly .c-hero-panel__overlay', 50, 'L');
-anime__punchin('.c-hero-panel__fly .c-hero-panel__xxl', 150, 'L');
-anime__punchin('.c-hero-panel__fly .c-hero-panel__eyebrow', 200, 'L');
+anime__fadeinX('.heropanel__fly .heropanel__button',2200, -50);
+anime__punchin('.heropanel__fly .heropanel__overlay', 50, 'L');
+anime__punchin('.heropanel__fly .heropanel__xxl', 150, 'L');
+anime__punchin('.heropanel__fly .heropanel__eyebrow', 200, 'L');
 
 // // PT
-// anime__blockoutText('.c-hero-panel__pt .c-hero-panel__content h4');
-// anime__blockoutText('.c-hero-panel__pt .c-hero-panel__content p', 500);
+// anime__blockoutText('.heropanel__pt .heropanel__content h4');
+// anime__blockoutText('.heropanel__pt .heropanel__content p', 500);
 
 // PULSE
-anime__isometricScroll('.c-hero-panel__pulse > .c-hero-panel__overlay');
+anime__isometricScroll('.heropanel__pulse > .heropanel__overlay');
