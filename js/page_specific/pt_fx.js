131f576aa6e64b04a398b98023b29ff6b93cc508
Andy Pearson <andy@londonparkour.com>
2020-06-09 14:47:44 +0100

    update

--- js/page_specific/pt_fx.js
+++ js/page_specific/pt_fx.js
@@ -2,7 +2,7 @@
  *  P.T. Anime FX
 */
 
-anime__punchin('.pt_hero .c-hero-panel__overlay', 50, 'T');
-anime__punchin('.pt_hero .c-hero-panel__xxl', 150, 'T');
-anime__punchin('.pt_hero .c-hero-panel__description', 200, 'T');
+anime__punchin('.pt_hero .heropanel__overlay', 50, 'T');
+anime__punchin('.pt_hero .heropanel__xxl', 150, 'T');
+anime__punchin('.pt_hero .heropanel__description', 200, 'T');
 anime__morphshapes('.pt_hero #shapeMorph #triangle');
