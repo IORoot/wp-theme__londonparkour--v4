31e9a6316c3c8982ff357d1e6d40dd5cb0819444
Andy Pearson <andy@londonparkour.com>
2020-07-09 14:06:27 +0100

    Instagram Pulse upgrade

--- inc/register_sidebars.php
+++ inc/register_sidebars.php
@@ -6,6 +6,7 @@
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
 function londonparkourv4_widgets_init() {
+
 	register_sidebar( array(
 		'name'          => esc_html__( 'Sidebar - All Posts', 'londonparkourv4' ),
 		'id'            => 'sidebar-1',
@@ -33,6 +34,16 @@ function londonparkourv4_widgets_init() {
 		'before_widget' => '<section id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</section>',
 		'before_title'  => '<h3 class="widget-title"><a href="/pulse/">',
+		'after_title'   => '</a></h3>',
+    ) );
+    
+    register_sidebar( array(
+		'name'          => esc_html__( 'Sidebar - Pulse IG Dashboard', 'londonparkourv4' ),
+		'id'            => 'sidebar-pulse-ig',
+		'description'   => esc_html__( 'Add widgets here.', 'londonparkourv4' ),
+		'before_widget' => '<section id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</section>',
+		'before_title'  => '<h3 class="widget-title"><a href="/pulse/ig/">',
 		'after_title'   => '</a></h3>',
     ) );
     
