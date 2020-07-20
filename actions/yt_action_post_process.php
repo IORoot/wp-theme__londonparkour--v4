a0b702eabee2d40e979cd18edba4c23f3f2cf501
Andy Pearson <andy@londonparkour.com>
2020-07-20 09:48:14 +0100

    Beetter way of updating menu pulse

--- actions/yt_action_post_process.php
+++ actions/yt_action_post_process.php
@@ -46,9 +46,24 @@ function update_main_menu_pulse_image($scrape)
     // Get the latest curated post image.
     $image_id = get_post_thumbnail_id($latest_post[0]->ID);
 
-    if ($image_id){
-        // Update the postmeta ID for the menu image.
-        update_metadata_by_mid('post', 115599, $image_id);
+
+    $menu = wp_get_nav_menu_object('main-menu');
+    $menu_items = wp_get_nav_menu_items($menu->term_id);
+
+    $pulse = null;
+    foreach($menu_items as $item) {
+        if ($item->post_name == 'parkour-pulse-3')     // post_name of parkour pulse.
+        {
+            $pulse = $item;
+            break;
+        }
+    }
+    
+    // $pulse_meta = get_post_meta($pulse->ID);
+    // $pulse_mc_image = $pulse_meta['mc_image'];
+
+    if ($image_id && $pulse->ID){
+        update_post_meta($pulse->ID, 'mc_image', $image_id);
     }
 
     return;
