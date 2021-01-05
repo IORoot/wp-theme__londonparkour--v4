1c29291326860c0bc6a4086bc414f26d2d2a85e7
Andy Pearson <andy@londonparkour.com>
2021-01-05 12:29:23 +0000

    Add tailwind

--- inc/enqueue_scripts.php
+++ inc/enqueue_scripts.php
@@ -1,7 +1,7 @@
 <?php
 
 function londonparkourv4_scripts() {
-	wp_enqueue_style( 'londonparkourv4-style', get_stylesheet_uri() );
+	wp_enqueue_style( 'londonparkourv4-style', get_stylesheet_uri() . '/sass/style.css' );
 	wp_enqueue_script( 'londonparkourv4-navigation', get_template_directory_uri() . '/js/navigation.js', array(), null, true );
 	wp_enqueue_script( 'londonparkourv4-progressbar', get_template_directory_uri() . '/js/progress_bar.js', array(), null, true );
 	wp_enqueue_script( 'londonparkourv4-lazysizes-bgset', get_template_directory_uri() . '/js/lazysizes_plugins/ls.bgset.min.js', array(), null, true );
