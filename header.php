7ea7cd6a70171dc83e332a44759318ee49bf9080
Andy Pearson <andy@londonparkour.com>
2021-02-07 12:45:22 +0000

    remove waste

--- header.php
+++ header.php
@@ -35,8 +35,4 @@
 
 			</nav>
 
-			<nav class="h-16 bg-black flex md:px-6">
-				<?php include( __DIR__.'/src/components/sub-menu/sub-menu.php' );  ?>
-			</nav>
-
 		</header>
--- src/hooks/menu_locations.php
+++ src/hooks/menu_locations.php
@@ -5,7 +5,6 @@ function register_custom_theme_menus()
     register_nav_menus(
         array(
             'main-right' => 'Social Media',
-            'sub-menu'   => 'Sub Menu'
         )
     );
 }
