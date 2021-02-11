1aa150c46ff1db4b98dcb061c046b61ebe32dfcd
Andy Pearson <andy@londonparkour.com>
2021-02-11 11:44:06 +0000

    Add sizes

--- src/hooks/remove_all_thumbnail_sizes.php
+++ src/hooks/remove_all_thumbnail_sizes.php
@@ -1,3 +1,14 @@
 <?php
 
-add_filter( 'intermediate_image_sizes', '__return_empty_array', 999);
+// add_filter( 'intermediate_image_sizes', '__return_empty_array', 999);
+add_filter( 'intermediate_image_sizes', 'image_sizes_to_generate', 999);
+
+function image_sizes_to_generate($default_sizes)
+{
+    $new_sizes = [
+        'thumbnail',
+        'medium',
+        'large'
+    ];
+    return $new_sizes;
+}
