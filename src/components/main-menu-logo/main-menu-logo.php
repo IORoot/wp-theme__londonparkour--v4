d5a5d82f29b3328f95f0228ad92b69a855646769
root <root@dev.londonparkour.com>
2021-02-22 17:10:06 +0000

    White logo

--- src/components/main-menu-logo/main-menu-logo.php
+++ src/components/main-menu-logo/main-menu-logo.php
@@ -1,3 +1,3 @@
 <a class="logo flex-1" href="https://londonparkour.com" >
-    <svg class="h-16 w-32 sm:w-48 fill-goo"><use xlink:href="#h-logo-text" /></svg>
+    <svg class="h-16 w-32 sm:w-48 fill-white"><use xlink:href="#h-logo-text" /></svg>
 </a>
